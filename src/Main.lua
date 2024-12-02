f4a1b2e1dafdf9da5b82f146b8941296dc4a2861
lfms6164 <morim.leonardo2@gmail.com>
2024-12-02 11:29:04 +0000

    Events cleanup

--- src/Main.lua
+++ src/Main.lua
@@ -5,50 +5,9 @@ LGRI.Main = {}
 
 local EM = EVENT_MANAGER
 
-local roleIcons = LGRI.icons.roleIcons
-
 function LGRI.Main.RegisterEvents()
-    EM:RegisterForEvent(LGRI.name .. "JoinedGroup", EVENT_GROUP_MEMBER_JOINED,
-        function(eventCode, memberCharacterName, memberDisplayName, isLocalPlayer)
-            if not isLocalPlayer then -- this only triggers when GroupSize > 2
-                d(memberDisplayName .. " has joined the group.")
-                LGRI.UI.AddGroupIcons()
-                return
-            end
-            LGRI.UI.playerFrame.RoleIcon:SetTexture(roleIcons[GetGroupMemberSelectedRole("player")])
-        end)
-    EM:RegisterForEvent(LGRI.name .. "RoleChanged", EVENT_GROUP_MEMBER_ROLE_CHANGED,
-        function(eventCode, unitTag, assignedRole)
-            LGRI.UI.AddGroupIcons()
-            if unitTag == GetGroupUnitTagByIndex(GetGroupIndexByUnitTag("player")) then
-                LGRI.UI.playerFrame.RoleIcon:SetTexture(roleIcons[assignedRole])
-                LGRI.UI.playerFrame.RoleIcon:SetHidden(assignedRole == 0)
-            end
-        end)
-    EM:RegisterForEvent(LGRI.name .. "LeftGroup", EVENT_GROUP_MEMBER_LEFT,
-        function(eventCode, memberCharacterName, groupLeaveReason, isLocalPlayer, isLeader, memberDisplayName,
-                 actionRequiredVote)
-            if not isLocalPlayer then
-                if GetGroupSize() == 4 then LGRI.UI.AddGroupIcons() end
-                return
-            end
-            LGRI.UI.playerFrame.RoleIcon:SetTexture(roleIcons[GetGroupMemberSelectedRole("player")])
-        end)
+    EM:RegisterForEvent(LGRI.name .. "JoinedGroup", EVENT_GROUP_MEMBER_JOINED, LGRI.UI.AddGroupIcons) -- this only triggers when GroupSize > 2
+    EM:RegisterForEvent(LGRI.name .. "RoleChanged", EVENT_GROUP_MEMBER_ROLE_CHANGED, LGRI.UI.AddGroupIcons)
+    EM:RegisterForEvent(LGRI.name .. "LeftGroup", EVENT_GROUP_MEMBER_LEFT, LGRI.UI.AddGroupIcons)
     EM:RegisterForEvent(LGRI.name .. "GroupUpdate", EVENT_GROUP_UPDATE, LGRI.UI.AddGroupIcons) -- Triggers when someone joins group or changes location
 end
-
-local function HideAndShowIcons()
-    local isVisible = LGRI.savedVars.visible
-    local state = isVisible and "hidden." or "shown."
-
-    -- Toggle visibility
-    local playerFrame = _G["MyFrame"]
-    if not playerFrame then LGRI.UI.BuildPlayerFrame() end
-    LGRI.UI.playerFrame.Frame:SetHidden(isVisible)
-
-    -- Update savedVars and print status
-    LGRI.savedVars.visible = not isVisible
-    d("LGRI: PlayerPanel icons " .. state)
-end
-
-SLASH_COMMANDS["/lgri"] = HideAndShowIcons
