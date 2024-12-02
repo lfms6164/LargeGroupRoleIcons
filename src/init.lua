a754801d4896f417dd60c189a9f904708704616b
lfms6164 <morim.leonardo2@gmail.com>
2024-12-02 11:28:36 +0000

    Remove settings, remove player icons panel

--- src/init.lua
+++ src/init.lua
@@ -1,26 +1,15 @@
 LargeGroupRoleIcons = LargeGroupRoleIcons or {}
 local LGRI = LargeGroupRoleIcons
 LGRI.name = "LargeGroupRoleIcons"
-LGRI.version = "0.2"
+LGRI.version = "0.3"
 
 local EM = EVENT_MANAGER
-
-local defaults = {
-	visible = true,
-	lockUI = true,
-	defaultPos = true,
-	MyFrameX = 0,
-	MyFrameY = 100,
-}
 
 function LGRI.OnAddOnLoaded(event, addonName)
 	if addonName ~= LGRI.name then return end
 	EM:UnregisterForEvent(LGRI.name, EVENT_ADD_ON_LOADED)
 
-	LGRI.savedVars = ZO_SavedVars:NewAccountWide("LargeGroupRoleIconsVars", 1, nil, defaults)
-
-	LGRI.Settings.CreateSettingsWindow()
-	if LGRI.savedVars.visible then LGRI.UI.BuildPlayerFrame() end
+    if GetGroupSize() > 0 then LGRI.UI.AddGroupIcons() end
 
 	LGRI.Main.RegisterEvents()
 end
